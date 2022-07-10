3ae2bf144835a3a7b7d8c4aae75d5081db20d319
VianneyMI <vianney.mixtur@outlook.fr>
2022-07-10 17:34:45 +0200

    Developed unit test and ensured it passes

--- backend/test/test_pricing.py
+++ backend/test/test_pricing.py
@@ -3,14 +3,14 @@
 from fastapi.testclient import TestClient
 from hypothesis import given, strategies as st
 
-from ..app.main import app
-from ..app.models.matching import InputMatchingAll
+from backend.app.main import app
+from backend.app.models.matching import InputMatchingAll
 
 client = TestClient(app)
 
 @given(st.builds(InputMatchingAll))
-def test_pricing_endpoint(body:InputMatchingAll):
+def test_pricing_endpoint(body:InputMatchingAll)->None:
     """Testes the pricing endpoints"""
 
-    response = client.post("/fuzzy-price/linear-reg", json=body.json())
+    response = client.post("/pricing/fuzzy-price/linear-reg", json=body.dict())
     assert response.status_code == 200
